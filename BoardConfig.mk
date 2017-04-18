fbaf425c9d9656d5db32f9b8006c6895afc8a3ee
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    kipper: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,9 +19,5 @@ include device/wileyfox/kipper/board/*.mk
 
 TARGET_BOARD_INFO_FILE := device/wileyfox/kipper/board-info.txt
 
-# inherit from the proprietary version
--include vendor/wileyfox/kipper/BoardConfigVendor.mk
-
-TARGET_HW_DISK_ENCRYPTION := true
-BOARD_HAVE_QCOM_FM := true
-BOARD_CHARGER_ENABLE_SUSPEND := true
+# Inherit from proprietary files
+include vendor/wileyfox/kipper/BoardConfigVendor.mk
