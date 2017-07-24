a1d41069e0cb8701df256e2013baf658f77f5c13
Michael Bestas <mkbestas@lineageos.org>
2017-07-24 20:29:06 +0300

    kipper: Remove QCPATH support
    Change-Id: I2488233eddbef2f24c9ef6cfec4866a2363a41c5

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,11 +8,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=9,9
 
 # RIL
-ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
-	$(LOCAL_PATH)/configs/data/NsrmConfiguration.xml:system/etc/dpm/nsrm/NsrmConfiguration.xml \
-    $(LOCAL_PATH)/configs/data/dpm.conf:system/etc/dpm/dpm.conf
-endif
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
